7fc93c8ee672d67da008a091e5fa23ee99e2d20f
DeletedC <CurtisWoodsArt@gmail.com>
2021-04-30 15:04:21 -0400

    Moved title to TitleBar component

---
+++ components/TitleBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TitleBar = props => {
+  return (
+    <View>
+      <Text>Shopping Reminder App</Text>
+    </View>
+  );
+};
+
+export default TitleBar;
