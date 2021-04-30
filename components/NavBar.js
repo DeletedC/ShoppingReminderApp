61fd1fe62c2fa053058d8e5b7dea0981e455d9a9
DeletedC <CurtisWoodsArt@gmail.com>
2021-04-30 15:51:44 -0400

    Pushed NavBar to bottom of screen

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const NavBar = props => {
   return (
-    <View>
+    <View style={styles.navBar}>
       <Button title="New" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  navBar: {
+      flex: 1,
+      justifyContent: 'flex-end',
+  }
+});
 
 export default NavBar;
